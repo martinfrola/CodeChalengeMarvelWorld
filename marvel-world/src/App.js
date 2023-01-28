a9d9f159a3ebdaaae18f4560fefef8c5f6f45225
Martin Frola <martin.flora@oversoft.net>
2023-01-27 21:01:11 -0300

    Prueba CI

--- marvel-world/src/App.js
+++ marvel-world/src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
-import Header from './components/generalComponents/Header';
-import Footer from './components/generalComponents/Footer'
-import './styles/App.scss'
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import Header from "./components/generalComponents/Header";
+import Footer from "./components/generalComponents/Footer";
+import "./styles/App.scss";
 
 function App() {
   return (
-    <div className="app">
+    <div>
       <Header />
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
       <Footer />
     </div>
   );
 }
 
 export default App;
-
